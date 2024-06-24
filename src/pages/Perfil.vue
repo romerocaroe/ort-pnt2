a9477ddc78c8a39dc32a8d5ad7fb3b4c0ce4febb
AntonellaDalPoggetto <luna.anto15@gmail.com>
2024-06-24 20:48:13 -0300

    agregar coleccion

--- src/pages/Perfil.vue
+++ src/pages/Perfil.vue
@@ -7,6 +7,17 @@
                 </q-item>
             </q-list>
         </div>
+        <!--TODO: crear nueva coleccion-->
+        
+        <form @submit.prevent="crearColeccion" class="coleccion-form q-gutter-md">
+            <input v-model="title" type="text" placeholder="Titulo" class="coleccion-input">
+            <input v-model="image" type="text" placeholder="link imagen" class="coleccion-input">
+        
+            <div class="q-gutter-sm justify-center">
+                <q-btn class="full-width" label="Crear" type="submit" color="primary"/>
+            </div>
+        </form>
+
         <!--TODO: separador-->
         <coleccion />
     </div>
@@ -14,7 +25,9 @@
 
 <script>
 import { useAuthStore } from '../stores/authStore'
+import { useArtStore } from 'src/stores/artStore'
 import Coleccion from '../components/Coleccion.vue'
+
 
 export default {
     name:'Perfil',
@@ -32,11 +45,26 @@ export default {
             const authStore = useAuthStore()
             this.user = authStore.user
             console.log(authStore.user)
-        }
+        },
+
+        crearColeccion(){
+            const artStore = useArtStore()
+
+            const nuevaColeccion = {
+                
+                title: this.title,
+           
+                image: this.image
+            };
+            artStore.addCollection(nuevoColeccion);
+            alert('Coleccion creada con exito')
+            this.$router.push({ name: 'Coleccion' })
+        
     },
     mounted(){
         this.getUser()
     }
+}
 }
 </script>
 
