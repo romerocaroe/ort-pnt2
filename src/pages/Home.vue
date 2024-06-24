ec75a1851f09ce07d2bf9561ae08a8263d31afaa
romerocaroe <romerocaroe@gmail.com>
2024-06-24 20:52:59 -0300

    Home responsive

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,13 +1,10 @@
 <template>
     <div class="row justify-center align-center">
-        <div >
-          
-          <CardObra    v-for="art in arts" :key="art" :art='art'/>
+        <div v-for="art in arts" :key="art">
             <div class="col-4 q-ma-sm">
-
+                <CardObra :art='art'/>
             </div>
         </div>
-
         <q-page-sticky position="bottom-right" :offset="[18, 18]">
             <q-btn fab icon="add" color="primary" />
         </q-page-sticky>
